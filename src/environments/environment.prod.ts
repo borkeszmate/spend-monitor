61094ee0064b469df76bb8f429154bfc887d2bab
Mate Borkesz <borkeszmate@gmail.com>
2018-10-30 17:03:50 +0100

    environment.prod.ts firebase credentials added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC9Y4cyWAJHr1_02eisUfrulMIgSD1pnuQ',
+    authDomain: 'spend-monitor-629f9.firebaseapp.com',
+    databaseURL: 'https://spend-monitor-629f9.firebaseio.com',
+    projectId: 'spend-monitor-629f9',
+    storageBucket: 'spend-monitor-629f9.appspot.com',
+    messagingSenderId: '296719070287'
+  }
 };
